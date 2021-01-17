ae7a8cab6744ba3909cb1484db0c8aa17acd32ce
andjelarajic <andjelarajic1@gmail.com>
2021-01-17 21:14:49 +0100

    added border

--- src/Projekt/Projekt.js
+++ src/Projekt/Projekt.js
@@ -8,12 +8,17 @@ import myProject from "../Images/Project3.jpg"
 const projekt = () => {
     return <div className="Project">
  <h1 className="Text">Gaming History!</h1>
+ <hr className="hr"/>
  <p className="AboutP">Gaming history is a responsiv website about games that were popular such as Super Mario and Zelda.</p>
  <img className="Image" src={myWork} alt="Game websida"></img>
+ <hr className="hr2"/>
   <h1 className="Text">Hotel Marina</h1>
+  <hr className="hr"/>
   <p className="AboutP"> This is an informative side where you can read more about a hotel in Dubai.</p>
   <img className="Image" src={myProject} alt="Hotel Marina"/>
+  <hr className="hr2"/>
   <h1 className="Text">Quire</h1>
+  <hr className="hr"/>
   <p className="AboutP">Quire is a webbased noteapp. The app can write, print, save and delete small notes when needed. All notes is saved in local storage.</p>
   <img className="Image" src={image} alt="Notebook"/>
     </div>
