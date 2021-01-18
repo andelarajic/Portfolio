e3219c7f7a700d7332b5483e828119e47d7ca5ff
andjelarajic <andjelarajic1@gmail.com>
2021-01-18 22:46:05 +0100

    responsive desing

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -9,16 +9,13 @@ const contact = () => {
     return <div className="Contact">
     <h1>Ok, let's create <br/>something great</h1>
     <p>If you like my work and have some cool project to work on, contact me through social sites listed below.</p>
-    <section className="social">
-		<ul className="social-set">
-			<li><a className="sociali" href="https://github.com/">
-			<FontAwesomeIcon className="Icons" icon={['fab', 'linkedin']} size="3x"/></a></li></ul>
-            <ul className="social-set">
-			<li><a className="sociali" href="https://www.instagram.com/">
-			<FontAwesomeIcon className="Icons" icon={['fab', 'github']} size="3x"/></a></li></ul>
-            <ul className="social-set">
-			<li><a className="sociali" href="https://www.google.com/search?q=media+queries&rlz=1C5CHFA_enSE904SE915&oq=media+q&aqs=chrome.1.69i57j0l5j0i395l4.4359j1j15&sourceid=chrome&ie=UTF-8">
-			<FontAwesomeIcon className="Icons" icon={['fab', 'twitter']} size="3x"/></a></li></ul>
+    <section className="icons">
+			<a className="active" href="https://github.com/">
+			<FontAwesomeIcon icon={['fab', 'linkedin']} size="3x"/></a>
+		    <a className="active" href="https://www.instagram.com/">
+			<FontAwesomeIcon icon={['fab', 'github']} size="3x"/></a>
+			<a className="active" href="https://www.google.com/search?q=media+queries&rlz=1C5CHFA_enSE904SE915&oq=media+q&aqs=chrome.1.69i57j0l5j0i395l4.4359j1j15&sourceid=chrome&ie=UTF-8">
+			<FontAwesomeIcon icon={['fab', 'twitter']} size="3x"/></a>
             </section>
     </div>
 };
