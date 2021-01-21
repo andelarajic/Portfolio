0ea19324555b1c5fb23c3df12be41f1c71b29e05
andjelarajic <andjelarajic1@gmail.com>
2021-01-21 18:06:58 +0100

    api

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,21 +5,41 @@ import "../../Components/Header/Header.css"
 import Post from "../Post/Post"
 
 class Header extends Component {
+    state = {
+        posts: []
+    }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                const posts = response.data.slice(0, 2);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Andela'
+                    }
+                })
+                this.setState ({posts: updatedPosts});
+               // console.log(response);
             });
 
     }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title = {post.title} author = {post.author}/>
+        })
     return  (
+        <div>
+        <section className="Posts">
+            {posts}
+        </section>
     <div className="Header1">
-     <Post/>
     <h1 className="HeaderH1">Andela Rajic</h1>
     <h2>Hi, I'm a Front-end Developer. I produce elegant,performant and accessible website.</h2>
     <button className= "button1">Check my work</button>
     </div>
+    </div>
+    
     )
 }};
 
