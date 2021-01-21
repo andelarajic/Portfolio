eaf5ca80e7077d714aa298e2c92cff600edc8bd7
andjelarajic <andjelarajic1@gmail.com>
2021-01-21 14:07:41 +0100

    changed css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,17 @@
+import { Component } from 'react';
 import React from "react"
 import "../../Components/Header/Header.css"
 
-const header = () => {
-    return <div className="Header1">
+class Header extends Component {
+    render () {
+    return  (
+    <div className="Header1">
     <h1>Andela Rajic</h1>
     <h2>Hi, I'm a Front-end Developer. I produce elegant,performant and accessible website.</h2>
     <button className= "button1">Check my work</button>
     </div>
-};
+    )
+}};
 
 
-export default header;
+export default Header;
