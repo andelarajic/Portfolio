986a4ec8393b45eb6485548b1c29563c64aa76ed
andjelarajic <andjelarajic1@gmail.com>
2021-01-21 15:00:37 +0100

    react rooter intall

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,22 @@
 import { Component } from 'react';
 import React from "react"
+import axios from 'axios';
 import "../../Components/Header/Header.css"
+import Post from "../Post/Post"
 
 class Header extends Component {
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response);
+            });
+
+    }
     render () {
     return  (
     <div className="Header1">
-    <h1>Andela Rajic</h1>
+     <Post/>
+    <h1 className="HeaderH1">Andela Rajic</h1>
     <h2>Hi, I'm a Front-end Developer. I produce elegant,performant and accessible website.</h2>
     <button className= "button1">Check my work</button>
     </div>
