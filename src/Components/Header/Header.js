ec4c2f777c3cc26cb594d9597008b3b8d5975997
andjelarajic <andjelarajic1@gmail.com>
2021-01-24 14:51:17 +0100

    api weather

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,47 +1,46 @@
-import { Component } from 'react';
-import React from "react"
+import React, {useState} from "react"
 import axios from 'axios';
 import "../../Components/Header/Header.css"
-import Post from "../Post/Post"
 
-class Header extends Component {
-    state = {
-        posts: []
-    }
 
-    componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 2);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Andela'
-                    }
-                })
-                this.setState ({posts: updatedPosts});
-               // console.log(response);
-            });
 
-    }
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post title = {post.title} author = {post.author}/>
+const Header = () => {
+    const [temperature, setTemperature] = useState("");
+    const [city, setCity] = useState("City");
+    const [desc, setDesc] =useState("");
+
+    const getWeatherData = (city) => {
+        axios({
+            method: "GET",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e0e9386da1077c731a4790d52287872a`
         })
-    return  (
+        .then((response) => {
+    console.log(response.data.main.temp);
+            setTemperature(response.data.main.temp - 273.15);
+            setDesc(response.data.weather[0].main);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
+    return (
         <div>
-        <section className="Posts">
-            {posts}
-        </section>
     <div className="Header1">
+   
+    {Math.round(temperature * 100) / 100} ℃ - {desc}
+
+    <input type="text" value={city} onChange={(e) =>setCity(e.target.value)}/>
+    <button onClick={() => {
+        getWeatherData(city);
+    }}> GET</button>
     <h1 className="HeaderH1">Andela Rajic</h1>
     <h2>Hi, I'm a Front-end Developer. I produce elegant,performant and accessible website.</h2>
     <button className= "button1">Check my work</button>
     </div>
     </div>
-    
     )
-}};
+};
 
 
 export default Header;
