a3170a7673dc197f2fd5e5e2c25572f11c217a86
andjelarajic <andjelarajic1@gmail.com>
2021-02-05 13:39:00 +0100

    changed About component

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -7,10 +7,11 @@ import myImage from "../../Images/MyImage.jpg"
 const aboutme = () => {
     return <div className="AboutMe">
      <img className="MyImage" src={myImage} alt="Myimage"></img>
-    <h1>About me</h1>
-    <p>Hi, I'am Andela, I'am 25 years old. I'am pasionate about learning new things and coding is so amazing, because there's always more to learn! I am always ready for new challenges and I am looking forward to it! 
+     <div className="AboutText">
+    <h1 className="AboutText1">About me</h1>
+    <p className="AboutText2">Hi, I'am Andela, I'am 25 years old. I'am pasionate about learning new things and coding is so amazing, because there's always more to learn! I am always ready for new challenges and I am looking forward to it! 
 </p>
-
+</div>
     </div>
 };
 
